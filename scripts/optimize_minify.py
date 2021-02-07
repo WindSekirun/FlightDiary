922a874d5ebfd3c2f024227a0a49dd82ddcf07aa
Pyxis <pyxis@uzuki.live>
2021-02-07 17:30:15 +0900

    fix minify doesn't working on build

--- scripts/optimize_minify.py
+++ scripts/optimize_minify.py
@@ -3,11 +3,12 @@ import json
 import os
 
 pwd = os.path.dirname(os.path.realpath(__file__))
-public_folder = os.path.join(os.path.realpath('..'), 'public')
+public_folder = os.path.join(os.path.realpath('.'), 'public')
 
-print('Minify JSON file')
+print(f'Minify JSON file in {public_folder}')
 
 for json_file in glob.glob(f'{public_folder}/**/*.json', recursive=True):
+    print(json_file)
     with open(json_file) as f:
         json_data = json.load(f)
         json_string = json.dumps(json_data, separators=(',', ":"))
